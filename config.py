b79653ce4a2f016c38a05cca8f69e6f520204c73
fucusy <fucus@qq.com>
2016-07-15 20:01:34 +0800

    update data path

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ class project:
     # the CASIA gait dataset B path, you can download the data from
     # CASIA website, the dirtory contains a lot sub dirtory
     # named such as 001,002...
-    casia_dataset_b_path = "/Users/fucus/Documents/irip/gait_recoginition/data/DatasetB/silhouettes"
+    casia_dataset_b_path = "%s/data/dataset_b/silhouettes" % project_path
 
-    casia_test_img = "%s/001/bg-01/000/001-bg-01-000-030.png" % casia_dataset_b_path
+    casia_test_img = "%s/001/bg-01/090/001-bg-01-090-038.png" % casia_dataset_b_path
 
     casia_test_img_dir = "%s/004/nm-01/090/" % casia_dataset_b_path
 
-    test_data_path = "%s/data" % project_path
+    test_data_path = "%s/data/test/" % project_path
 
     if not os.path.exists(test_data_path):
         os.makedirs(test_data_path)
